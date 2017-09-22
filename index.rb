6bb7bf6c362ee81fe2c48759f733cf38efa33556
Dhron Joshi <dhron.joshi@uwaterloo.ca>
2017-09-22 08:15:23 -0400

    add challenge code

--- index.rb
+++ index.rb
@@ -1,55 +1,82 @@
 require 'http'
 require 'json'
-#need to loop for more than just the first page
+require 'sinatra'
 
-class validator
-	def initialize(json)
-		@json = json
-		@required; #boolean
-		@type; #array of strings
-		@length; #array [max, min]
+#Include type checking for boolean
+module Boolean; end
+class TrueClass; include Boolean; end
+class FalseClass; include Boolean; end
+
+class Validator
+	def initialize()
+		@customers #the current list of customers
+		@validation #the current json rules for validation
+		@invalid = { "invalid_customers" => [] } #will be a subset of all customers read through
+		@pageInfo
 	end
-	def validate(input)
-		
+
+	def read(url, page) #get the data
+
+		requestUrl = url + '?page=' + page.to_s
+		response = HTTP.get(requestUrl).to_s
+		jsonInput = JSON.parse(response)
+		@validation = jsonInput["validations"]
+		@customers = jsonInput["customers"]
+		@pageInfo = jsonInput["pagination"]
+	end
+
+	def output()
+		puts @validation
+		puts @customers
+		puts @pageInfo
+	end
+
+	def validate()
+		@customers.each do | customer |
+			invalidJSON = { "id" => customer["id"], "invalid_fields" => [] }
+			@validation.each do | rule |
+				key, value = rule.first
+				invalidFlag = false
+				invalidFlag = value["required"] && !customer[key] 
+				if value["type"] #if a type is specified, check the type
+					types = {"number" => Numeric, "string" => String, "boolean" => Boolean}
+					invalidFlag = !customer[key].is_a?(types[value["type"]])
+				end
+				if value["length"] && customer[key]
+					range = value["length"]
+					max = range["max"]
+					min = range["min"]
+					if(max && min)
+						invalidFlag = customer[key].length < min || customer[key].length > max
+					elsif max
+						invalidFlag = customer[key].length > max
+					else
+						invalidFlag = customer[key].length < min
+					end
+				end
+				if invalidFlag
+					invalidJSON["invalid_fields"].push(key.to_s)
+				end
+			end
+			if invalidJSON["invalid_fields"].size > 0
+					@invalid["invalid_customers"].push(invalidJSON)
+			end
+		end
+	end
+	def returnInvalid()
+		return @invalid.to_json
 	end
 end
 
-res = HTTP.get('https://backend-challenge-winter-2017.herokuapp.com/customers.json').to_s
-resJSON = JSON.parse(res);
 
-validation = resJSON["validations"]
-customers = resJSON["customers"]
-pageInfo = resJSON["pagination"]
+machine = Validator.new()
 
-puts validation
-puts customers
-puts pageInfo
+get '/' do
+	(1..5).each do | page |
+	machine.read('https://backend-challenge-winter-2017.herokuapp.com/customers.json', page)
+	machine.validate()
+	end
+	machine.returnInvalid()
+end
 
-validatorMachine = new validator(validation)
-
-
-#you could make a validation object with certain properties
-#by reading and assigning the properties in the validation obj
-#test the cases against the object
-
-#create a tree out of the information and then
-#do a traversal?
-
-
-
-#need to write a way to validate a json object
-#general structure:
-#'name': {
-#   'required': (BOOL), (defaults to false if not provided)
-#   'type': ("boolean", "number", or "string") (if not provided, any type is valid
-#   'length': {
-#       'min': (int),
-#       'max': (int)
-#   }
-#}
-
-#so they provide you with a schema, and then you validate the customer submission based on the schema
-
-#also create an api:
-#POST /schema, this returns the schema being used
-#POST /submission, this returns the valid or invalid json
+#machine.returnInvalid()
